0f8c5040bf25987812750f4baf0f13ab3a548913
Charles Liang <chaoli@microsoft.com>
2017-11-21 15:29:45 +0800

    add enter

--- testdropbox/Startup.cs
+++ testdropbox/Startup.cs
@@ -14,6 +14,7 @@ namespace testdropbox
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
